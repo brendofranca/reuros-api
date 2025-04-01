8c813895ad29879b01fff52d8e334eddd2e2a65d
brendo.franca <brendosbf@gmail.com>
2025-04-01 13:14:12 +0200

    Update main.go

--- src/main.go
+++ src/main.go
@@ -20,9 +20,22 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/currency/", handlers.GetCurrencyRates)
 
+	corsMiddleware := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			if r.Method == http.MethodOptions {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+
 	server := &http.Server{
 		Addr:    ":" + port,
-		Handler: mux,
+		Handler: corsMiddleware(mux),
 	}
 
 	go func() {
