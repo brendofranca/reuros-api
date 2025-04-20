fde82e80f185466c587353a4d53b1753ae695549
brendo.franca <brendosbf@gmail.com>
2025-04-20 10:26:18 +0200

    Remove unused currency and user models, handlers, and services; add user_request.go file

--- src/models/currency_model.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type CurrencyRate struct {
-	Base  string             `json:"base_code"`
-	Date  string             `json:"time_last_update_utc"`
-	Rates map[string]float64 `json:"conversion_rates"`
-}
